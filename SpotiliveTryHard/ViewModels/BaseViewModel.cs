ff50f4763ad2f422a6429e0c384533b189889cb5
SthaN42 <nathan.guerin@etu.unilim.fr>
2022-03-03 09:33:02 +0100

    ajout baseViewModel

---
+++ SpotiliveTryHard/ViewModels/BaseViewModel.cs
@@ -0,0 +1,51 @@
+﻿using spotilive.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace spotilive.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Dictionary<string, object> properties = new Dictionary<string, object>();
+
+        protected void SetValue<T>(T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!properties.ContainsKey(propertyName))
+            {
+                properties.Add(propertyName, default(T));
+            }
+
+            var oldValue = GetValue<T>(propertyName);
+            if (!EqualityComparer<T>.Default.Equals(oldValue, value))
+            {
+                properties[propertyName] = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        protected T GetValue<T>([CallerMemberName] string propertyName = null)
+        {
+            if (!properties.ContainsKey(propertyName))
+            {
+                return default(T);
+            }
+            else
+            {
+                return (T)properties[propertyName];
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
