62dbf7fdb18379ed96885f46ba2aebdecf779987
marcuzzi1 <quentin.marcuzzi@etu.unilim.fr>
2022-03-03 09:15:26 +0100

    Add project files.

---
+++ SpotiliveTryHard/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpotiliveTryHard
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SpotiliveTryHard/Pages/PlayListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpotiliveTryHard.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayListPage : ContentPage
+    {
+        public PlayListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
