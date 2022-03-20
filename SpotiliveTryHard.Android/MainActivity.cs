c0662e1aaa8e5593528ff4dfa9585cba2352413c
SthaN42 <nathan.guerin@etu.unilim.fr>
2022-03-20 21:39:03 +0100

    modification du logo

--- SpotiliveTryHard.Android/MainActivity.cs
+++ SpotiliveTryHard.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using AndroidX.AppCompat.App;
 
 namespace SpotiliveTryHard.Droid
 {
-    [Activity(Label = "SpotiliveTryHard", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SPOTiLIVE", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
