6f8b21bea0105cbd5890d5de6445269420f04a48
SthaN42 <nathan.guerin@etu.unilim.fr>
2022-03-21 10:25:49 +0100

    fix de l'appui sur les recommendations

--- SpotiliveTryHard/Pages/RecommendedPage.xaml.cs
+++ SpotiliveTryHard/Pages/RecommendedPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpotiliveTryHard.Models;
 using SpotiliveTryHard.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,6 +17,17 @@ namespace SpotiliveTryHard.Pages
         {
             BindingContext = new RecommendedViewModel();
             InitializeComponent();
+        }
+
+        private async void ViewCell_Tapped(object sender, SelectionChangedEventArgs e)
+        {
+            if (!(e.CurrentSelection.FirstOrDefault() is Playlist playlist))
+            {
+                return;
+            }
+
+            (sender as CollectionView).SelectedItem = null;
+            await Navigation.PushAsync(new PlaylistDetailsPage(playlist));
         }
     }
 }
